c343caebcd2b89da09b09133c8c15d9919498050
azarrias <adolfo.zarrias@gmail.com>
2018-06-08 12:05:41 +0200

    Add minimal source code to check that the dependencies are properly resolved

---
+++ src/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "SDL2/SDL.h"
+
+int main(){
+    if (SDL_Init(SDL_INIT_VIDEO) != 0){
+        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+    SDL_Quit();
+    return 0;
+}
